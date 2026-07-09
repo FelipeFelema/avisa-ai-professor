4e7c040ccefcaf9e2300f7da740cdc1995221b52
Felipe <felipe.felema78514@gmail.com>
2026-07-08 22:27:26 -0300

    chore(mobile): configure eslint and prettier

--- mobile/src/providers/AppProvider.tsx
+++ mobile/src/providers/AppProvider.tsx
@@ -1,12 +1,10 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { PropsWithChildren } from "react";
+import { QueryClientProvider } from '@tanstack/react-query';
+import { PropsWithChildren } from 'react';
 
-import { queryClient } from "../config/query-client";
+import { queryClient } from '../config/query-client';
 
 type AppProviderProps = PropsWithChildren;
 
 export function AppProvider({ children }: AppProviderProps) {
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
